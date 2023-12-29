95135c736078787cb201a30ae756a1ef03db53cb
SantamariaCodes <diego.santamaria75@gmail.com>
2023-12-29 08:54:15 -0600

    first commit

---
+++ MovieCatalog app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieCatalog app
+//
+//  Created by Diego Santamaria on 29/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieCatalog app/MovieCatalog_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCatalog_appApp.swift
+//  MovieCatalog app
+//
+//  Created by Diego Santamaria on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieCatalog_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
